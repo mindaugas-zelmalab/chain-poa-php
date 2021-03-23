d6b2358cd9363f48e0d5f7d17bda4cd0e0d15dd9
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-03-23 17:29:18 +0500

    GenesisTx update

--- src/Transactions/Flags/GenesisTx.php
+++ src/Transactions/Flags/GenesisTx.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace ForwardBlock\Chain\PoA\Transactions\Flags;
 
+use ForwardBlock\Protocol\Exception\TxDecodeException;
 use ForwardBlock\Protocol\Transactions\AbstractPreparedTx;
 
 /**
@@ -11,4 +12,37 @@ use ForwardBlock\Protocol\Transactions\AbstractPreparedTx;
  */
 class GenesisTx extends AbstractPreparedTx
 {
+    /** @var array */
+    public array $chainMasters = [];
+
+    /**
+     * @throws TxDecodeException
+     */
+    protected function decodeCallback(): void
+    {
+        if ($this->data) {
+            $keys = $this->data->raw();
+            $chainMasters = str_split($keys, 32);
+            if (count($chainMasters) !== 5) {
+                throw TxDecodeException::Incomplete($this,
+                    sprintf('Genesis tx must have precisely 5 chain masters; Got %d', count($chainMasters))
+                );
+            }
+
+            $this->chainMasters = $chainMasters;
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function array(): array
+    {
+        $partial = parent::array();
+        foreach ($this->chainMasters as $chainMaster) {
+            $partial[] = bin2hex($chainMaster);
+        }
+
+        return $partial;
+    }
 }
